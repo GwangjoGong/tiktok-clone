573c0b6ef7162c9915a860d5a8120c5c81706217
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-08 15:56:47 +0900

    #4.4 Username Screen

---
+++ lib/features/authentication/username_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class UsernameScreen extends StatefulWidget {
+  const UsernameScreen({super.key});
+
+  @override
+  State<UsernameScreen> createState() => _UsernameScreenState();
+}
+
+class _UsernameScreenState extends State<UsernameScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Sign up',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size32,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v20,
+            const Text(
+              "Create username",
+              style: TextStyle(
+                fontSize: Sizes.size20,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v10,
+            const Text(
+              "You can always change this later.",
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v32,
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                hintText: "Username",
+                hintStyle: TextStyle(color: Colors.grey.shade400),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v32,
+            FractionallySizedBox(
+              widthFactor: 1,
+              child: AnimatedContainer(
+                curve: Curves.easeInOut,
+                duration: const Duration(milliseconds: 200),
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size16,
+                ),
+                decoration: BoxDecoration(
+                  color: _username.isEmpty
+                      ? Colors.grey.shade200
+                      : Theme.of(context).primaryColor,
+                  borderRadius:
+                      const BorderRadius.all(Radius.circular(Sizes.size4)),
+                ),
+                child: Text(
+                  "Next",
+                  style: TextStyle(
+                    color:
+                        _username.isEmpty ? Colors.grey.shade500 : Colors.white,
+                    fontSize: Sizes.size16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
