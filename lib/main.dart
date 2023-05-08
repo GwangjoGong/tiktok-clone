64f1db89b6aac93ab1c59708d886093586d45f8d
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-08 14:29:26 +0900

    #4.0 Sign Up Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_clone/constants/gaps.dart';
-import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/screens/sign_up_screen.dart';
 
 void main() {
   runApp(const TiktokApp());
@@ -13,21 +12,11 @@ class TiktokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primaryColor: const Color(0xFFE9435A),
-        ),
-        home: Padding(
-          padding: const EdgeInsets.all(Sizes.size14),
-          child: Container(
-            child: Row(
-              children: const [
-                Text('Hello'),
-                Gaps.h20,
-                Text('World!'),
-              ],
-            ),
-          ),
-        ));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: const SignUpScreen(),
+    );
   }
 }
