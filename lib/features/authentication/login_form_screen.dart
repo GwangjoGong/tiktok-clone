d2fa24159f6d34d40eeade5d2b98890687d2082e
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-08 18:21:50 +0900

    #4.9 Login Form

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-class LoginFormScreen extends StatelessWidget {
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Map<String, String> formData = {};
+
+  void _login(BuildContext context) {
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if (isValid) {
+      _formKey.currentState?.save();
+      // Move to onboarding
+    }
+  }
+
+  void _setFormData({required String key, String? value}) {
+    if (value != null) {
+      formData[key] = value;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log in"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Gaps.v28,
+              TextFormField(
+                decoration: const InputDecoration(hintText: "Email"),
+                onSaved: (value) => _setFormData(key: "email", value: value),
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: const InputDecoration(hintText: "Password"),
+                onSaved: (value) => _setFormData(key: "password", value: value),
+              ),
+              Gaps.v28,
+              FormButton(disabled: false, text: "Log in", onTap: _login)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
