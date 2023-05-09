e447a31d5442a4fdcdcbdbe1b97ea968c961787e
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-09 16:11:28 +0900

    #6.3 NavigationBar

--- lib/features/main/main_navigation_screen.dart
+++ lib/features/main/main_navigation_screen.dart
@@ -12,9 +12,9 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   final screens = [
     const Center(child: Text("Home")),
     const Center(child: Text("Settings")),
-    const Center(child: Text("Activity")),
-    const Center(child: Text("Browse")),
-    const Center(child: Text("Community")),
+    // const Center(child: Text("Activity")),
+    // const Center(child: Text("Browse")),
+    // const Center(child: Text("Community")),
   ];
 
   int _selectedIndex = 0;
@@ -25,34 +25,18 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onTap,
-        // selectedItemColor: Theme.of(context).primaryColor,
-        items: const [
-          BottomNavigationBarItem(
+      bottomNavigationBar: NavigationBar(
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: _onTap,
+        destinations: const [
+          NavigationDestination(
             icon: FaIcon(FontAwesomeIcons.house),
             label: "Home",
-            backgroundColor: Colors.amber,
           ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.personRunning),
-            label: "Activity",
-            backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.firefoxBrowser),
-            label: "Browse",
-            backgroundColor: Colors.blue,
-          ),
-          BottomNavigationBarItem(
-              icon: FaIcon(FontAwesomeIcons.users),
-              label: "Community",
-              backgroundColor: Colors.purple),
-          BottomNavigationBarItem(
+          NavigationDestination(
             icon: FaIcon(FontAwesomeIcons.gear),
             label: "Settings",
-            backgroundColor: Colors.red,
           ),
         ],
       ),
