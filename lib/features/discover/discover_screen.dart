40ab724ac46e3fffe72b9c1cd1a5ea3dddca963d
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-12 16:19:17 +0900

    #9.4 Grid Item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class DiscoverScreen extends StatelessWidget {
@@ -54,14 +55,72 @@ class DiscoverScreen extends StatelessWidget {
               ),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-                crossAxisSpacing: Sizes.size10,
-                mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 16,
+                crossAxisSpacing: Sizes.size8,
+                mainAxisSpacing: Sizes.size8,
+                childAspectRatio: 9 / 20.5,
               ),
               itemBuilder: (context, index) {
-                return Container(
-                  color: Colors.teal,
-                  child: Text('Top $index'),
+                return Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 9 / 16,
+                      child: Container(
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.size4),
+                        ),
+                        child: FadeInImage.assetNetwork(
+                          fit: BoxFit.cover,
+                          placeholder: "assets/images/sample.jpg",
+                          image:
+                              "https://source.unsplash.com/random/200x${355 + index}/?dance",
+                        ),
+                      ),
+                    ),
+                    Gaps.v10,
+                    const Text(
+                      "This is a very long caption for my tiktok that i am uploading for just now and i am very happy to be here",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: Sizes.size16 + Sizes.size2,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Gaps.v10,
+                    DefaultTextStyle(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey.shade600,
+                      ),
+                      child: Row(
+                        children: [
+                          const CircleAvatar(
+                            radius: Sizes.size14,
+                            backgroundImage: NetworkImage(
+                                "https://source.unsplash.com/random/200x200/?dance"),
+                          ),
+                          Gaps.h6,
+                          const Expanded(
+                            child: Text(
+                              "Gwangjo Gong",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Gaps.h6,
+                          Icon(
+                            Icons.favorite_border,
+                            color: Colors.grey.shade600,
+                            size: Sizes.size16,
+                          ),
+                          const Text(
+                            "2.0M",
+                          )
+                        ],
+                      ),
+                    )
+                  ],
                 );
               },
             ),
