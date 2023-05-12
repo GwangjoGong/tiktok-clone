292cb4b721d99f7138c9383344969833cf2dd3cc
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-12 18:41:27 +0900

    #10.5 SlideTransition

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -21,11 +21,46 @@ class _ActivityScreenState extends State<ActivityScreen>
     "6",
   ];
 
+  final List<Map<String, dynamic>> _tabs = [
+    {
+      "title": "All activity",
+      "icon": FontAwesomeIcons.solidMessage,
+    },
+    {
+      "title": "Likes",
+      "icon": FontAwesomeIcons.solidHeart,
+    },
+    {
+      "title": "Comments",
+      "icon": FontAwesomeIcons.solidComments,
+    },
+    {
+      "title": "Mentions",
+      "icon": FontAwesomeIcons.at,
+    },
+    {
+      "title": "Followers",
+      "icon": FontAwesomeIcons.solidUser,
+    },
+    {
+      "title": "From TikTok",
+      "icon": FontAwesomeIcons.tiktok,
+    }
+  ];
+
   late final AnimationController _animationController = AnimationController(
       duration: const Duration(milliseconds: 300), vsync: this);
 
   late final Animation<double> _turnAnimation =
       Tween(begin: 0.0, end: 0.5).animate(
+    CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.fastOutSlowIn,
+    ),
+  );
+
+  late final Animation<Offset> _slideAnimation =
+      Tween(begin: const Offset(0, -1), end: Offset.zero).animate(
     CurvedAnimation(
       parent: _animationController,
       curve: Curves.fastOutSlowIn,
@@ -69,82 +104,123 @@ class _ActivityScreenState extends State<ActivityScreen>
           ),
         ),
       ),
-      body: ListView(
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size14,
-              horizontal: Sizes.size16,
-            ),
-            child: Text(
-              "New",
-              style: TextStyle(
-                color: Colors.grey.shade600,
-                fontWeight: FontWeight.w600,
-                fontSize: Sizes.size16,
-              ),
-            ),
-          ),
-          for (var notification in _notifications)
-            Dismissible(
-              key: Key(notification),
-              onDismissed: (direction) {
-                _onDismissed(notification);
-              },
-              background: Container(
-                color: Colors.red,
-                padding: const EdgeInsets.only(left: Sizes.size16),
-                alignment: Alignment.centerLeft,
-                child: const FaIcon(
-                  FontAwesomeIcons.trash,
-                  color: Colors.white,
+          ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size14,
+                  horizontal: Sizes.size16,
                 ),
-              ),
-              child: ListTile(
-                minVerticalPadding: Sizes.size16,
-                leading: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.grey.shade300)),
-                  width: Sizes.size52,
-                  child: const Center(
-                      child: FaIcon(
-                    FontAwesomeIcons.bell,
-                    color: Colors.black,
-                  )),
-                ),
-                title: RichText(
-                  text: const TextSpan(
-                    text: "Account updates: ",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black,
-                      fontSize: Sizes.size16,
-                    ),
-                    children: [
-                      TextSpan(
-                        text: "Upload longer videos ",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                      TextSpan(
-                        text: "1h",
-                        style: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
+                child: Text(
+                  "New",
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontWeight: FontWeight.w600,
+                    fontSize: Sizes.size16,
                   ),
                 ),
-                trailing: FaIcon(
-                  FontAwesomeIcons.chevronRight,
-                  size: Sizes.size16,
-                  color: Colors.grey.shade600,
+              ),
+              for (var notification in _notifications)
+                Dismissible(
+                  key: Key(notification),
+                  onDismissed: (direction) {
+                    _onDismissed(notification);
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    padding: const EdgeInsets.only(left: Sizes.size16),
+                    alignment: Alignment.centerLeft,
+                    child: const FaIcon(
+                      FontAwesomeIcons.trash,
+                      color: Colors.white,
+                    ),
+                  ),
+                  child: ListTile(
+                    minVerticalPadding: Sizes.size16,
+                    leading: Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.grey.shade300)),
+                      width: Sizes.size52,
+                      child: const Center(
+                          child: FaIcon(
+                        FontAwesomeIcons.bell,
+                        color: Colors.black,
+                      )),
+                    ),
+                    title: RichText(
+                      text: const TextSpan(
+                        text: "Account updates: ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                          fontSize: Sizes.size16,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Upload longer videos ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "1h",
+                            style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    trailing: FaIcon(
+                      FontAwesomeIcons.chevronRight,
+                      size: Sizes.size16,
+                      color: Colors.grey.shade600,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+          SlideTransition(
+            position: _slideAnimation,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(Sizes.size6),
+                  bottomRight: Radius.circular(Sizes.size6),
                 ),
               ),
-            )
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  for (var tab in _tabs)
+                    ListTile(
+                      title: Row(
+                        children: [
+                          FaIcon(
+                            tab["icon"],
+                            color: Colors.black,
+                            size: Sizes.size16,
+                          ),
+                          Gaps.h20,
+                          Text(
+                            tab["title"],
+                            style: const TextStyle(
+                              fontSize: Sizes.size16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
