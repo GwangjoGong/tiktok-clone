dd6c750674695bdc27e97cdf75c464ced6b7c52a
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-15 14:01:08 +0900

    #11.3 AnimatedList part Two

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/chat_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -11,8 +12,63 @@ class ChatsScreen extends StatefulWidget {
 class _ChatsScreenState extends State<ChatsScreen> {
   final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
 
+  final Duration _duration = const Duration(milliseconds: 200);
+
+  void _onChatTap() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const ChatScreen()));
+  }
+
   void _addItem() {
-    _listKey.currentState?.insertItem(0);
+    _listKey.currentState?.insertItem(0, duration: _duration);
+  }
+
+  void _deleteItem(int index) {
+    _listKey.currentState?.removeItem(index, duration: _duration,
+        (context, animation) {
+      final curvedAnimation = animation.drive(
+        CurveTween(curve: Curves.fastOutSlowIn),
+      );
+
+      return FadeTransition(
+        opacity: curvedAnimation,
+        child: SizeTransition(
+          sizeFactor: curvedAnimation,
+          child: _makeTile(index),
+        ),
+      );
+    });
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onTap: _onChatTap,
+      onLongPress: () => _deleteItem(index),
+      leading: const CircleAvatar(
+        radius: Sizes.size32,
+        child: Text('A'),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "AntonioBM $index",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade400,
+              fontSize: Sizes.size14,
+            ),
+          )
+        ],
+      ),
+      subtitle: const Text("Say hi to AntonioBM"),
+    );
   }
 
   @override
@@ -43,32 +99,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
             opacity: curvedAnimation,
             child: SizeTransition(
               sizeFactor: curvedAnimation,
-              child: ListTile(
-                leading: const CircleAvatar(
-                  radius: Sizes.size32,
-                  child: Text('A'),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      "AntonioBM $index",
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      "2:16 PM",
-                      style: TextStyle(
-                        color: Colors.grey.shade400,
-                        fontSize: Sizes.size14,
-                      ),
-                    )
-                  ],
-                ),
-                subtitle: const Text("Say hi to AntonioBM"),
-              ),
+              child: _makeTile(index),
             ),
           );
         },
