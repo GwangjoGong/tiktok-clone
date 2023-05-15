a732488052ec30815571109158e4a332ffde16fd
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-15 16:31:31 +0900

    #12.2 SliverAppBar

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -10,25 +10,38 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          backgroundColor: Colors.transparent,
-          expandedHeight: 200,
-          flexibleSpace: FlexibleSpaceBar(
-            stretchModes: const [
-              StretchMode.zoomBackground,
-              StretchMode.blurBackground,
-            ],
-            background:
-                Image.asset("assets/images/sample.jpg", fit: BoxFit.cover),
-            title: const Text("Hello"),
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            // backgroundColor: Colors.transparent,
+            expandedHeight: 200,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              stretchModes: const [
+                StretchMode.zoomBackground,
+                StretchMode.blurBackground,
+              ],
+              background:
+                  Image.asset("assets/images/sample.jpg", fit: BoxFit.cover),
+              title: const Text("Hello"),
+            ),
           ),
-          floating: true,
-          stretch: true,
-          pinned: true,
-        ),
-      ],
+          SliverFixedExtentList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 50,
+              (context, index) {
+                return Container(
+                  color: Colors.primaries[index % Colors.primaries.length],
+                  height: 100,
+                  child: Center(child: Text("Item $index")),
+                );
+              },
+            ),
+            itemExtent: 100,
+          )
+        ],
+      ),
     );
   }
 }
