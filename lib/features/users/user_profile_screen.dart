2b1b0cbf313a342d9d63991b14a46d2c8bf482fc
GwangjoGong <dwc05101@kaist.ac.kr>
2023-05-15 18:08:38 +0900

    #12.6 TabBar

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -14,172 +14,238 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: const Text("Gwangjo Gong"),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.settings),
-                splashRadius: Sizes.size20,
-                onPressed: () {},
-              ),
-            ],
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                const CircleAvatar(
-                  radius: Sizes.size52,
-                  foregroundImage: NetworkImage("https://picsum.photos/200"),
+      body: DefaultTabController(
+        length: 2,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: const Text("Gwangjo Gong"),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.settings),
+                  splashRadius: Sizes.size20,
+                  onPressed: () {},
                 ),
-                Gaps.v20,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "@Gwangjo Gong",
-                      style: TextStyle(fontWeight: FontWeight.w600),
-                    ),
-                    Gaps.h5,
-                    FaIcon(
-                      FontAwesomeIcons.solidCircleCheck,
-                      size: Sizes.size12,
-                      color: Colors.blue.shade200,
-                    ),
-                  ],
-                ),
-                Gaps.v24,
-                SizedBox(
-                  height: 45,
-                  child: Row(
+              ],
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  const CircleAvatar(
+                    radius: Sizes.size52,
+                    foregroundImage: NetworkImage("https://picsum.photos/200"),
+                  ),
+                  Gaps.v20,
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Column(
-                        children: [
-                          const Text(
-                            "37",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: Sizes.size20,
-                            ),
-                          ),
-                          Gaps.v4,
-                          Text(
-                            "Following",
-                            style: TextStyle(color: Colors.grey.shade500),
-                          )
-                        ],
+                      const Text(
+                        "@Gwangjo Gong",
+                        style: TextStyle(fontWeight: FontWeight.w600),
                       ),
-                      VerticalDivider(
-                        color: Colors.grey.shade200,
-                        width: 30,
-                        thickness: 1,
-                        indent: 10,
-                        endIndent: 10,
-                      ),
-                      Column(
-                        children: [
-                          const Text(
-                            "10.5M",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: Sizes.size20,
-                            ),
-                          ),
-                          Gaps.v4,
-                          Text(
-                            "Followers",
-                            style: TextStyle(color: Colors.grey.shade500),
-                          )
-                        ],
-                      ),
-                      VerticalDivider(
-                        color: Colors.grey.shade200,
-                        width: 30,
-                        thickness: 1,
-                        indent: 10,
-                        endIndent: 10,
-                      ),
-                      Column(
-                        children: [
-                          const Text(
-                            "149.3M",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: Sizes.size20,
-                            ),
-                          ),
-                          Gaps.v4,
-                          Text(
-                            "Likes",
-                            style: TextStyle(color: Colors.grey.shade500),
-                          )
-                        ],
+                      Gaps.h5,
+                      FaIcon(
+                        FontAwesomeIcons.solidCircleCheck,
+                        size: Sizes.size12,
+                        color: Colors.blue.shade200,
                       ),
                     ],
                   ),
-                ),
-                Gaps.v10,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 52,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: Sizes.size64,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(
-                          Sizes.size2,
+                  Gaps.v24,
+                  SizedBox(
+                    height: 45,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            const Text(
+                              "37",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size20,
+                              ),
+                            ),
+                            Gaps.v4,
+                            Text(
+                              "Following",
+                              style: TextStyle(color: Colors.grey.shade500),
+                            )
+                          ],
                         ),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          "Follow",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: Sizes.size16,
-                            color: Colors.white,
+                        VerticalDivider(
+                          color: Colors.grey.shade200,
+                          width: 30,
+                          thickness: 1,
+                          indent: 10,
+                          endIndent: 10,
+                        ),
+                        Column(
+                          children: [
+                            const Text(
+                              "10.5M",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size20,
+                              ),
+                            ),
+                            Gaps.v4,
+                            Text(
+                              "Followers",
+                              style: TextStyle(color: Colors.grey.shade500),
+                            )
+                          ],
+                        ),
+                        VerticalDivider(
+                          color: Colors.grey.shade200,
+                          width: 30,
+                          thickness: 1,
+                          indent: 10,
+                          endIndent: 10,
+                        ),
+                        Column(
+                          children: [
+                            const Text(
+                              "149.3M",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size20,
+                              ),
+                            ),
+                            Gaps.v4,
+                            Text(
+                              "Likes",
+                              style: TextStyle(color: Colors.grey.shade500),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Gaps.v10,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 52,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: Sizes.size64,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(
+                            Sizes.size2,
+                          ),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Follow",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: Sizes.size16,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Gaps.h6,
-                    Container(
-                      height: 52,
-                      width: 52,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey.shade300),
-                        borderRadius: BorderRadius.circular(Sizes.size2),
-                      ),
-                      child: const Center(
-                        child: FaIcon(
-                          FontAwesomeIcons.youtube,
-                          size: Sizes.size24,
+                      Gaps.h6,
+                      Container(
+                        height: 52,
+                        width: 52,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey.shade300),
+                          borderRadius: BorderRadius.circular(Sizes.size2),
+                        ),
+                        child: const Center(
+                          child: FaIcon(
+                            FontAwesomeIcons.youtube,
+                            size: Sizes.size24,
+                          ),
                         ),
                       ),
-                    ),
-                    Gaps.h6,
-                    Container(
-                      height: 52,
-                      width: 52,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey.shade300),
-                        borderRadius: BorderRadius.circular(Sizes.size2),
+                      Gaps.h6,
+                      Container(
+                        height: 52,
+                        width: 52,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey.shade300),
+                          borderRadius: BorderRadius.circular(Sizes.size2),
+                        ),
+                        child: const Icon(
+                          Icons.arrow_drop_down,
+                          size: Sizes.size28,
+                        ),
                       ),
-                      child: const Icon(
-                        Icons.arrow_drop_down,
-                        size: Sizes.size28,
+                    ],
+                  ),
+                  Gaps.v14,
+                  const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: Sizes.size24,
+                    ),
+                    child: Text(
+                      "All highlights and where to watch live matches on FIFA+",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Gaps.v14,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      FaIcon(
+                        FontAwesomeIcons.link,
+                        size: Sizes.size12,
+                      ),
+                      Gaps.h6,
+                      Text(
+                        "https://www.fifa.com/fifaplus/en/home",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      )
+                    ],
+                  ),
+                  Gaps.v14,
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.symmetric(
+                        horizontal: BorderSide(color: Colors.grey.shade200),
                       ),
                     ),
-                  ],
-                )
-              ],
+                    child: const TabBar(
+                      tabs: [
+                        Tab(
+                          icon: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: Sizes.size16,
+                            ),
+                            child: FaIcon(
+                              Icons.grid_4x4_rounded,
+                              color: Colors.black,
+                              size: Sizes.size20,
+                            ),
+                          ),
+                        ),
+                        Tab(
+                          icon: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: Sizes.size16,
+                            ),
+                            child: FaIcon(
+                              FontAwesomeIcons.heart,
+                              color: Colors.black,
+                              size: Sizes.size20,
+                            ),
+                          ),
+                        ),
+                      ],
+                      indicatorColor: Colors.black,
+                      indicatorSize: TabBarIndicatorSize.label,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
